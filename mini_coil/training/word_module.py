6d16fd14e5ea3ec4557a31e559c4c46ec0ab10c2
generall <andrey@vasnetsov.com>
2025-01-12 00:08:09 +0100

    rollback contrastive changes

--- mini_coil/training/word_module.py
+++ mini_coil/training/word_module.py
@@ -1,3 +1,4 @@
+from random import random
 from typing import Dict
 
 import lightning as L
@@ -6,77 +7,8 @@ import torch
 from torch import optim
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 
-from mini_coil.contrastive_loss import ContrastiveLoss
 from mini_coil.model.cosine_loss import CosineLoss
 from mini_coil.model.word_encoder import WordEncoder
-
-
-class ContrastiveWordModule(L.LightningModule):
-    def __init__(self, encoder: WordEncoder, lr: float = 2e-3, factor: float = 0.5, patience: int = 5):
-        super().__init__()
-        self.encoder = encoder
-        self.loss = ContrastiveLoss()
-        self.lr = lr
-        self.factor = factor
-        self.patience = patience
-
-    def configure_optimizers(self):
-        optimizer = optim.Adam(self.parameters(), lr=self.lr)
-        return {
-            "optimizer": optimizer,
-            "lr_scheduler": {
-                "scheduler": ReduceLROnPlateau(
-                    optimizer,
-                    mode="min",
-                    factor=self.factor,
-                    patience=self.patience,
-                    verbose=True,
-                    threshold=1e-4
-                ),
-                "monitor": "val_loss",
-            },
-        }
-
-    def training_step(self, batch, batch_idx):
-        emb = torch.from_numpy(batch["word_embeddings"]).float().to(self.device)
-        pairs = torch.from_numpy(batch["pairs"]).long().to(self.device)
-        labels = torch.from_numpy(batch["labels"]).float().to(self.device)
-        encoded = self.encoder(emb)
-        loss_val = self.loss(
-            encoded,
-            pairs,
-            labels,
-            torch.zeros(len(encoded), dtype=torch.int64, device=self.device),
-
-        )
-        self.log(
-            "train_loss",
-            loss_val,
-            on_epoch=True,
-            on_step=False,
-            prog_bar=True,
-            logger=True,
-            batch_size=emb.size(0)
-        )
-        return loss_val
-
-    def validation_step(self, batch, batch_idx):
-        emb = torch.from_numpy(batch["word_embeddings"]).float().to(self.device)
-        pairs = torch.from_numpy(batch["pairs"]).long().to(self.device)
-        labels = torch.from_numpy(batch["labels"]).float().to(self.device)
-        encoded = self.encoder(emb)
-        loss_val = self.loss(
-            encoded,
-            pairs,
-            labels,
-            torch.zeros(len(encoded), dtype=torch.int64, device=self.device)
-        )
-        self.log(
-            "val_loss",
-            loss_val,
-            batch_size=emb.size(0)
-        )
-        return loss_val
 
 
 class WordModule(L.LightningModule):
@@ -150,15 +82,7 @@ class WordModule(L.LightningModule):
             batch_idx: int
     ):
         loss = self.encode_decode_loss(batch)
-        batch_size = batch['word_embeddings'].shape[0]
-        self.log(
-            "train_loss",
-            loss,
-            on_epoch=True,
-            on_step=False,
-            prog_bar=True,
-            logger=True,
-            batch_size=batch_size)
+        self.log("train_loss", loss, on_epoch=True, on_step=False, prog_bar=True, logger=True)
 
         return loss
 
